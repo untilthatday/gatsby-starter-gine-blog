5ad986192fffd3706ceae615370913604f7aa333
untilthatday <51884935+untilthatday@users.noreply.github.com>
2022-01-18 12:53:17 +0800

    change notion config url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/gine-blog-v2-58556b252e864111b3ce872b124d0940#6c22830de46c49128858d73fdbb62678"
+                configTable: "https://www.notion.so/39b75b8c7c8648d3be7a2137c49b604b?v=6a31d8079cfa4d96aefc6f43bd96ac69"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/gine-blog-v2-58556b252e864111b3ce872b124d0940#a477421f372b478496851eedd1042883"
+                configTable: "https://www.notion.so/4d49662a527d45e8b7bd8e9b014144f4?v=a1a4203a8623434781150de17e57b9b4"
             }
         }
     ],
